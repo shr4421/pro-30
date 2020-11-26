72b740bdeec6fbff07afdd364e6422c621e5c82f
shr4421 <73051737+shr4421@users.noreply.github.com>
2020-11-26 20:50:31 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+      
+       if (this.body.speed<3){
+            this.body.display();
+       }
+        else{
+
+            World.remove(world,this.body);
+            push();
+            this.visibility=this.visibility-5;
+            tint(225,this.visibility);
+            pop();
+
+        }
+
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
